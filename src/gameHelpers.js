c3abf726d5f16a2e09d3eb38680d87c85b8c65ba
TrieuBui1006 <buiductrieu1006@gmail.com>
2020-07-09 01:44:56 +0200

    add movement

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -5,3 +5,24 @@ export const createStage = () =>
   Array.from(Array(STAGE_HEIGHT), () =>
     new Array(STAGE_WIDTH).fill([0, 'clear'])
   )
+
+export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
+  for (let y = 0; y < player.tetromino.length; y += 1) {
+    for (let x = 0; x < player.tetromino[0].length; x += 1) {
+      //1. Check that we are on an actual tertomino cell
+      if (player.tetromino[y][x] !== 0) {
+        if (
+          //2. check that our move is inside the game areas height (y)
+          !stage[y + player.pos.y + moveY] ||
+          //3. check that our move is inside the game areas width (x)
+          !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+          //4. check if the cell is "clear"
+          stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !==
+            'clear'
+        ) {
+          return true
+        }
+      }
+    }
+  }
+}
